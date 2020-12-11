10fea2a55c4d570931791dcf311fd35b5f09b22d
johndheem <johndheem@blendernetworks.com>
2020-12-10 20:28:54 -0400

    host and port is an application property setting

--- src/main/java/com/spring/examples/elasticsearch/configuration/ElasticsearchClientConfiguration.java
+++ src/main/java/com/spring/examples/elasticsearch/configuration/ElasticsearchClientConfiguration.java
@@ -1,18 +1,24 @@
 package com.spring.examples.elasticsearch.configuration;
 
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
 @Configuration
-public class ElasticsearchClientConfiguration  extends AbstractElasticsearchConfiguration {
+public class ElasticsearchClientConfiguration extends AbstractElasticsearchConfiguration {
+  @Value("${elasticsearh.hostandport}")
+  private String hostAndPort;
 
   @Override
   @Bean
   public RestHighLevelClient elasticsearchClient() {
 
-    final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-        .connectedTo("localhost:9200")
-        .build();
+    final ClientConfiguration clientConfiguration =
+        ClientConfiguration.builder().connectedTo(hostAndPort).build();
 
     return RestClients.create(clientConfiguration).rest();
   }
