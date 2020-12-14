1149e5fa0956378caad998341e5c202301a6c499
johndheem <johndheem@blendernetworks.com>
2020-12-13 22:20:41 -0400

    Need to figure out ElasticSearchRepositories

--- src/main/java/com/spring/examples/elasticsearch/domain/User.java
+++ src/main/java/com/spring/examples/elasticsearch/domain/User.java
@@ -1,11 +1,14 @@
 package com.spring.examples.elasticsearch.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
 @Document(indexName = "user")
 public class User {
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
 }
---
+++ src/main/java/com/spring/examples/elasticsearch/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.spring.examples.elasticsearch.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends org.elasticsearch.repositories.Repository {
+
+}
