5acdaf3c8e196e60d935cd8a049cb69ebb7a98b7
johndheem <johndheem@blendernetworks.com>
2020-12-19 21:39:42 -0400

    Need to figure out the endpoints/reports how to create activites -> reservation of books

--- src/main/java/com/spring/examples/elasticsearch/service/StartUpService.java
+++ src/main/java/com/spring/examples/elasticsearch/service/StartUpService.java
@@ -6,9 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import jdk.internal.net.http.common.Pair;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
@@ -31,12 +35,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StartUpService {
   private final ResourceLoader resourceLoader;
-
   private final RestHighLevelClient elasticsearchClient;
 
   @PostConstruct
   private void initializeData() throws IOException, ParseException {
-    BulkRequest bulkRequest = new BulkRequest();
+    BulkRequest userBulkRequest = new BulkRequest();
 
     if (!elasticsearchClient
         .indices()
@@ -48,19 +51,19 @@ public class StartUpService {
           RequestOptions.DEFAULT);
     }
 
-    bulkRequest.add(createUserRequest("John", "Doe"));
-    bulkRequest.add(createUserRequest("Jane", "Doe"));
-    bulkRequest.add(createUserRequest("Mark", "Doe"));
-    bulkRequest.add(createUserRequest("Mills", "Doe"));
-    bulkRequest.add(createUserRequest("Ian", "Deer"));
-    bulkRequest.add(createUserRequest("Anna", "Deer"));
-    bulkRequest.add(createUserRequest("Anthony", "Deer"));
-    bulkRequest.add(createUserRequest("Josh", "Smith"));
-    bulkRequest.add(createUserRequest("Jenny", "Smith"));
-    bulkRequest.add(createUserRequest("Jack", "Mason"));
-    bulkRequest.add(createUserRequest("Alex", "Mason"));
+    userBulkRequest.add(createUserRequest("John", "Doe"));
+    userBulkRequest.add(createUserRequest("Jane", "Doe"));
+    userBulkRequest.add(createUserRequest("Mark", "Doe"));
+    userBulkRequest.add(createUserRequest("Mills", "Doe"));
+    userBulkRequest.add(createUserRequest("Ian", "Deer"));
+    userBulkRequest.add(createUserRequest("Anna", "Deer"));
+    userBulkRequest.add(createUserRequest("Anthony", "Deer"));
+    userBulkRequest.add(createUserRequest("Josh", "Smith"));
+    userBulkRequest.add(createUserRequest("Jenny", "Smith"));
+    userBulkRequest.add(createUserRequest("Jack", "Mason"));
+    userBulkRequest.add(createUserRequest("Alex", "Mason"));
 
-    elasticsearchClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+    elasticsearchClient.bulk(userBulkRequest, RequestOptions.DEFAULT);
 
     if (!elasticsearchClient
         .indices()
@@ -74,7 +77,7 @@ public class StartUpService {
           RequestOptions.DEFAULT);
     }
     // save the library right away.
-    bulkRequest.add(
+    userBulkRequest.add(
         new IndexRequest("library")
             .source(
                 XContentType.JSON,
@@ -109,31 +112,46 @@ public class StartUpService {
     if (!elasticsearchClient
         .indices()
         .exists(new GetIndexRequest("book"), RequestOptions.DEFAULT)) {
-      elasticsearchClient
-          .indices()
-          .create(new CreateIndexRequest("book"), RequestOptions.DEFAULT);
+      elasticsearchClient.indices().create(new CreateIndexRequest("book"), RequestOptions.DEFAULT);
     } else {
       elasticsearchClient.deleteByQuery(
           new DeleteByQueryRequest("book").setQuery(QueryBuilders.matchAllQuery()),
           RequestOptions.DEFAULT);
     }
 
+    BulkRequest bookBulkRequest = new BulkRequest();
+
     Resource resource = resourceLoader.getResource("classpath:timetop100novels.csv");
     BufferedReader csvReader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+    Integer lineNum = 0;
     String line;
+
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy");
+    Map<Integer, Pair<List<String>, Boolean>> books = new HashMap<>();
     while ((line = csvReader.readLine()) != null) {
       if (!line.equals("Title,Author,Year\n")) {
         String[] data = line.split(",");
-        bulkRequest.add(
+        books.put(lineNum, new Pair<>(Arrays.asList(data.clone()), true));
+        bookBulkRequest.add(
             createBookRequest(
                 data[0], data[1], simpleDateFormat.parse(""), true, null, library.getId()));
       }
+      lineNum++;
     }
 
     // randomly assigned users and books and actions
-
     csvReader.close();
+
+    // how am ig going to do it
+    // GET All userIds -> Create a list
+    //  Create a Map of book <Int, Pair<List<String>, Boolean>>
+
+    // Create
+
+   // create 5000 activities lol this users are book addicts! -> they would only keep it for a maximum 3 days! I need to figure out reservations!
+    // int i = 0;
+    // while i <=5000
+      // get a random book and
 
     // if activity index then create one, else delete all data and insert new data
 
