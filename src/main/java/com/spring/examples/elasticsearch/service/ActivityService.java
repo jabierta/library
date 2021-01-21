f9980814580aac390d387b4d9d644ade7b29f341
johndheem <johndheem@blendernetworks.com>
2021-01-20 22:48:12 -0400

    /getFavouriteBook complete and support methods from ActivityService and BookService classes for this endpoint

--- src/main/java/com/spring/examples/elasticsearch/service/ActivityService.java
+++ src/main/java/com/spring/examples/elasticsearch/service/ActivityService.java
@@ -1,8 +1,17 @@
 package com.spring.examples.elasticsearch.service;
 
+import java.io.IOException;
 import java.util.Calendar;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.BucketOrder;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,21 +23,43 @@ public class ActivityService {
     long startDate;
     long endDate;
     if (month == null) {
-      int currentMonth = month - 1;
       Calendar calendar = Calendar.getInstance();
-      calendar.set(year, 0);
+      calendar.set(year, Calendar.JANUARY, 1);
       startDate = calendar.getTimeInMillis();
-      calendar.set(year, 11);
+      calendar.set(year, Calendar.DECEMBER, 31);
       endDate = calendar.getTimeInMillis();
     } else {
       int currentMonth = month - 1;
       Calendar calendar = Calendar.getInstance();
-      calendar.set(year, (month - 1));
+      calendar.set(year, currentMonth, 1);
       startDate = calendar.getTimeInMillis();
-      calendar.set(year, month);
+      calendar.set(year, currentMonth + 1, 1);
       endDate = calendar.getTimeInMillis();
     }
 
-    return null;
+    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+    searchSourceBuilder.query(
+        QueryBuilders.boolQuery()
+            .must(QueryBuilders.matchQuery("userId", userId))
+            .must(QueryBuilders.rangeQuery("activityDate").gte(startDate).lte(endDate)));
+    searchSourceBuilder.aggregation(
+        AggregationBuilders.terms("by_books")
+            .field("bookId.keyword")
+            .order(BucketOrder.count(false))
+            .size(1));
+
+    SearchRequest searchRequest = new SearchRequest();
+    searchRequest.source(searchSourceBuilder);
+
+    try {
+      SearchResponse searchResponse =
+          elasticsearchClient.search(searchRequest, RequestOptions.DEFAULT);
+
+      Terms topBookAgg = searchResponse.getAggregations().get("by_books");
+
+      return topBookAgg.getBuckets().get(0).getKeyAsString();
+    } catch (IOException e) {
+      return null;
+    }
   }
 }
--- src/main/java/com/spring/examples/elasticsearch/service/BookService.java
+++ src/main/java/com/spring/examples/elasticsearch/service/BookService.java
@@ -2,7 +2,8 @@ package com.spring.examples.elasticsearch.service;
 
 import com.spring.examples.elasticsearch.domain.Book;
 import java.io.IOException;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.action.get.GetRequest;
@@ -21,18 +22,21 @@ public class BookService {
       GetResponse getResponse =
           elasticSearchClient.get(new GetRequest("book", bookId), RequestOptions.DEFAULT);
       return sourceAsMapToBook(bookId, getResponse.getSourceAsMap());
-    } catch (IOException e) {
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
       return null;
     }
   }
 
-  public Book sourceAsMapToBook(String bookId, Map<String, Object> sourceAsMap) {
+  public Book sourceAsMapToBook(String bookId, Map<String, Object> sourceAsMap)
+      throws ParseException {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     Book book = new Book();
     book.setId(bookId);
     book.setAuthor((String) sourceAsMap.get("author"));
-    book.setYear((Date) sourceAsMap.get("year"));
+    book.setYear(simpleDateFormat.parse((String) sourceAsMap.get("year")));
     book.setIsAvailableToBorrow((Boolean) sourceAsMap.get("isAvailableToBorrow"));
-    book.setNextAvailability((Date) sourceAsMap.get("nextAvailability"));
+    book.setNextAvailability(simpleDateFormat.parse((String) sourceAsMap.get("nextAvailability")));
     book.setLibraryId((String) sourceAsMap.get("libraryId"));
 
     return book;
