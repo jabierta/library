4759f88cbf3eb36e8c7eb97e0b4d21ddf82d29a2
johndheem <johndheem@blendernetworks.com>
2021-01-20 21:15:04 -0400

    Created a sourceMap to Book in Book Service, created find in BookService.java

--- src/main/java/com/spring/examples/elasticsearch/service/BookService.java
+++ src/main/java/com/spring/examples/elasticsearch/service/BookService.java
@@ -1,12 +1,41 @@
 package com.spring.examples.elasticsearch.service;
 
 import com.spring.examples.elasticsearch.domain.Book;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookService {
-  // TODO: Create this method body!
+  private final RestHighLevelClient elasticSearchClient;
+
   public Book find(String bookId) {
-    return null;
+    try {
+      GetResponse getResponse =
+          elasticSearchClient.get(new GetRequest("book", bookId), RequestOptions.DEFAULT);
+      return sourceAsMapToBook(bookId, getResponse.getSourceAsMap());
+    } catch (IOException e) {
+      // Maybe we should throw a CannotFindException
+      return null;
+    }
+  }
+
+  public Book sourceAsMapToBook(String bookId, Map<String, Object> sourceAsMap) {
+    Book book = new Book();
+    book.setId(bookId);
+    book.setAuthor((String) sourceAsMap.get("author"));
+    book.setYear((Date) sourceAsMap.get("year"));
+    book.setIsAvailableToBorrow((Boolean) sourceAsMap.get("isAvailableToBorrow"));
+    book.setNextAvailability((Date) sourceAsMap.get("nextAvailability"));
+    book.setLibraryId((String) sourceAsMap.get("libraryId"));
+
+    return book;
   }
 }
